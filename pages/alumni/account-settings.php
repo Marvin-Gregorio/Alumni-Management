d3c8dbc7ccbbd150826ad379ee93b8ee62eca59b
Leofer Dela Cruz <Leoferma099@gmail.com>
2023-02-20 23:35:24 +0800

    changing frontend in account settings

--- pages/alumni/account-settings.php
+++ pages/alumni/account-settings.php
@@ -41,216 +41,228 @@ crossorigin="anonymous" referrerpolicy="no-referrer" />
 <?php include('header.php') ?>
 
 <!-- This is Main -->
-<div class="bg-white mt-2 container-fluid" id="main">
+<div class="container-fluid" style="padding: 20px;">
     <div class="row">
-        <div class="col-sm-4">
-            <div>
-          <img src="../../profileImg/profile.jpg"class="w-100 img img-thumbnail" id="profileImg">
-          <button class="btn btn-primary form-control mt-2" onclick="clickImage()">Change Profile</button>     
-        </div>
-        <div class="mt-4">
-          <h5>Change Password:</h5>
-          <div class="pt-2">
-            <label>Old Password: </label>
-            <input type="password" id="old_pass" class="form-control">
+        <div class="col-md-3">
+          <div class="card">
+            <div class="card-body">
+              <div class="text-center">
+                <img src="../../profileImg/profile.jpg" class="img-thumbnail" width="50%" id="profileImg">
+                <br>
+                <button class="btn btn-info text-white mt-2" onclick="clickImage()"><b>Change Profile</b></button>     
+              </div>
+              <div class="row">
+                <div class="col-md-12">
+                <label>Old Password: </label>
+                <input type="password" id="old_pass" class="form-control form-control-sm">
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-12">
+                <label>New Password: </label>
+                <input type="password" id="new_pass" class="form-control form-control-sm">
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-12">
+                <label>Confirm Password: </label>
+                <input type="password" id="confirm_pass" class="form-control form-control-sm">
+                </div>
+              </div>
+                <div class="text-end">
+                <button onclick="updatePassword()" class="btn btn-info text-white mt-2"><b>Update Password</b></button>
+                </div>
+            </div>
           </div>
-          <div class="pt-2">
-            <label>New Password: </label>
-            <input type="password" id="new_pass" class="form-control">
-          </div>
-          <div class="pt-2">
-            <label>Confirm Password: </label>
-            <input type="password" id="confirm_pass" class="form-control">
-          </div>
-          <button onclick="updatePassword()" class="btn btn-primary mt-2 form-control">Update Password</button>
         </div>
-        </div>
-        <div class="col-sm-8">
-            <div>
-          <h2>Personal Information:</h2>
-          <div class="container-fluid">
+        <div class="col-sm-9">
+          <div class="card">
+            <div class="card-body">
             <div class="row">
+              <h5><b>Personal Information</b></h5>
               <div class="col-sm-4">
                 <label>First Name:</label>
-                <input class="form-control" id="fname" value="fda">
+                <input class="form-control form-control-sm" id="fname" value="fda">
               </div>
               <div class="col-sm-4">
                 <label>Middle Name:</label>
-                <input class="form-control" id="mname" value="fdasf">
+                <input class="form-control form-control-sm" id="mname" value="fdasf">
               </div>
               <div class="col-sm-4">
                 <label>Last Name:</label>
-                <input class="form-control" id="lname" value="fdasf">
+                <input class="form-control form-control-sm" id="lname" value="fdasf">
               </div>
             </div>
-            <div class="mt-2">
+            <div class="row">
+              <div class="col-md-6">
               <label>Email: </label>
-              <input type="text" id="p_email" value="fdasf" class="form-control">
-            </div>
-            <div class="mt-2">
+              <input type="text" id="p_email" value="fdasf" class="form-control form-control-sm">
+              </div>
+              <div class="col-md-3">
               <label>Cellphone Number:</label>
-              <input type="text" id="p_cp_number" value="fdasf" class="form-control">
-            </div>
-            <div class="mt-2">
+              <input type="text" id="p_cp_number" value="fdasf" class="form-control form-control-sm">
+              </div>
+              <div class="col-md-3">
               <label>Birth Date:</label>
-              <input type="date" id="b_date" class="form-control">
+              <input type="date" id="b_date" class="form-control form-control-sm">
+              </div>
             </div>
-            <div class="mt-2">
+            <div class="row">
+              <div class="col-md-6">
               <label>Username:</label>
-              <input type="text" id="uname" value="fdasf" class="form-control">
-            </div>
-            <div class="mt-2">
+              <input type="text" id="uname" value="fdasf" class="form-control form-control-sm">
+              </div>
+              <div class="col-md-3">
               <label>Gender:</label>
-              <select type="text" id="gender" class="form-control">
+              <select type="text" id="gender" class="form-control form-control-sm">
                 <option value="MALE">Male</option>
                 <option value="FEMALE">Female</option>
               </select>
-            </div>
-            <div class="mt-2">
+              </div>
+              <div class="col-md-3">
               <label>Status:</label>
-              <select type="text" id="status" class="form-control">
+              <select type="text" id="status" class="form-control form-control-sm">
                 <option value="SINGLE">Single</option>
                 <option value="MARRIED">Married</option>
                 <option value="WIDOW">Widow</option>
               </select>
+              </div>
+              <div class="text-end">
+              <button onclick="updatePersonalDetails()" class="btn btn-info text-white mt-2">Update Profile Info</button>  
+              </div>
             </div>
-          </div>
-          <button onclick="updatePersonalDetails()" class="btn btn-primary form-control mt-2">Update Profile Info</button>  
-        </div>
-
-        <div class="mt-4">
-          <h2>Educational Information:</h2>
-          <div class="container-fluid">
-            <div class="container">
-              <h5 class="pt-2">Elementary (Grade 1-6)</h5>
-              <div class="row">
-                <div class="col-sm-6">
-                  <label>School Name:</label>
-                  <input class="form-control" id="elem_school">
-                </div>
-                <div class="col-sm-6">
-                  <label>Year Graduated:</label>
-                  <input class="form-control" id="elem_year">
-                </div>
+            <div class="row">
+              <h5>Educational Background:</h5>
+              <div class="col-md-4">
+              <span><b>Elementary (Grade 1-6)</b></span>
+              <label>School Name:</label>
+              <input class="form-control form-control-sm" id="elem_school">
               </div>
-              <h5 class="pt-2">Junior High (Grade 7-10)</h5>
-              <div class="row">
-                <div class="col-sm-6">
-                  <label>School Name:</label>
-                  <input class="form-control" id="junior_school">
-                </div>
-                <div class="col-sm-6">
-                  <label>Year Graduated:</label>
-                  <input class="form-control" id="junior_year">
-                </div>
+              <div class="col-md-2">
+              <label>Year:</label>
+              <input class="form-control form-control-sm" id="elem_year">
               </div>
-              <h5 class="pt-2">Senior High (Grade 11-12)</h5>
-              <div class="row">
-                <div class="col-sm-4">
-                  <label>Course:</label>
-                  <select id="senior_course" class="form-control">
+              <div class="col-md-4">
+              <span><b>Junior Highschool (Grade 7-10)</b></span>
+              <label>School Name:</label>
+              <input class="form-control form-control-sm" id="junior_school">
+              </div>
+              <div class="col-md-2">
+              <label>Year Graduated:</label>
+              <input class="form-control form-control-sm" id="junior_year">
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-4">
+              <span><b>Senior Highschool (Grade 11-12)</b></span>
+              <label>Course:</label>
+                  <select id="senior_course" class="form-control form-control-sm">
                     <option value="BSCS">TVL</option>
                     <option value="BSOA">ABM</option>
                     <option value="BLIS">HUMMS</option>
                     <option value="BSIT">GAS</option>
                   </select>
-                </div>
-                <div class="col-sm-4">
-                  <label>School Name:</label>
-                  <input class="form-control" id="senior_school">
-                </div>
-                <div class="col-sm-4">
-                  <label>Year Graduated:</label>
-                  <input class="form-control" id="senior_year">
-                </div>
               </div>
-              <h5 class="pt-2">Undergraduate</h5>
-              <div class="row">
-                <div class="col-sm-4">
-                  <label>Course:</label>
-                  <select id="college_course" class="form-control">
+              <div class="col-md-4">
+              <label>School Name:</label>
+              <input class="form-control form-control-sm" id="senior_school">
+              </div>
+              <div class="col-md-4">
+              <label>Year Graduated:</label>
+              <input class="form-control form-control-sm" id="senior_year">
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-4">
+                <span><b>Undergraduate</b></span>
+              <label>Course:</label>
+                  <select id="college_course" class="form-control form-control-sm">
                     <option value="BSCS">BSCS</option>
                     <option value="BSOA">BSOA</option>
                     <option value="BLIS">BLIS</option>
                     <option value="BSIT">BSIT</option>
                   </select>
-                </div>
-                <div class="col-sm-4">
-                  <label>School Name:</label>
-                  <input class="form-control" id="college_school">
-                </div>
-                <div class="col-sm-4">
-                  <label>Year Graduated:</label>
-                  <input class="form-control" id="college_year">
-                </div>
               </div>
-              <h5 class="pt-2">Masteral</h5>
-              <div class="row">
-                <div class="col-sm-4">
-                  <label>Course:</label>
-                  <select id="master_course" class="form-control">
+              <div class="col-md-4">
+              <label>School Name:</label>
+              <input class="form-control form-control-sm" id="college_school">
+              </div>
+              <div class="col-md-4">
+              <label>Year Graduated:</label>
+              <input class="form-control form-control-sm" id="college_year">
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-4">
+              <span><b>Masteral</b></span>
+              <label>Course:</label>
+                  <select id="master_course" class="form-control form-control-sm">
                     <option value="MSCS">MSCS</option>
                     <option value="MSOA">MSOA</option>
                     <option value="MLIS">MLIS</option>
                     <option value="MSIT">MSIT</option>
                   </select>
-                </div>
-                <div class="col-sm-4">
-                  <label>School Name:</label>
-                  <input class="form-control" id="master_school">
-                </div>
-                <div class="col-sm-4">
-                  <label>Year Graduated:</label>
-                  <input class="form-control" id="master_year">
-                </div>
               </div>
-              <h5 class="pt-2">Doctoral</h5>
-              <div class="row">
-                <div class="col-sm-4">
-                  <label>Course:</label>
-                  <select id="doctor_course" class="form-control">
+              <div class="col-md-4">
+              <label>School Name:</label>
+              <input class="form-control form-control-sm" id="master_school">
+              </div>
+              <div class="col-md-4">
+              <label>Year Graduated:</label>
+              <input class="form-control form-control-sm" id="master_year">
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-4">
+              <span><b>Doctorate</b></span>
+              <label>Course:</label>
+                  <select id="doctor_course" class="form-control form-control-sm">
                     <option value="DSCS">DSCS</option>
                     <option value="DSOA">DSOA</option>
                     <option value="DLIS">DLIS</option>
                     <option value="DSIT">DSIT</option>
                   </select>
-                </div>
-                <div class="col-sm-4">
-                  <label>School Name:</label>
-                  <input class="form-control" id="doctor_school">
-                </div>
-                <div class="col-sm-4">
-                  <label>Year Graduated:</label>
-                  <input class="form-control" id="doctor_year">
-                </div>
+              </div>
+              <div class="col-md-4">
+              <label>School Name:</label>
+                  <input class="form-control form-control-sm" id="doctor_school">
+              </div>
+              <div class="col-md-4">
+              <label>Year Graduated:</label>
+                  <input class="form-control form-control-sm" id="doctor_year">
               </div>
             </div>
-            <button onclick="updateEducationalDetails()" class="btn btn-primary form-control mt-2">Update Educational Info</button> 
-          </div>  
-
-          <div class="mt-4">
-          <h2>Employment Information:</h2>
-          <div class="container-fluid">
-            <div class="mt-2">
+            <div class="text-end">
+            <button onclick="updateEducationalDetails()" class="btn btn-info text-white mt-2">Update Educational Info</button>
+            </div>
+            <div class="row">
+              <h5><b>Employment Status</b></h5>
+              <div class="col-md-12">
               <label>Company Name: </label>
-              <input type="text" id="company_name" value="" class="form-control">
+              <input type="text" id="company_name" value="" class="form-control form-control-sm">
+              </div>
             </div>
-            <div class="mt-2">
-              <label>Company Number:</label>
-              <input type="text" id="company_number" value="" class="form-control">
-            </div>
-            <div class="mt-2">
-              <label>Company Address: </label>
-              <input type="text" id="company_address" value="" class="form-control">
-            </div>
-            <div class="mt-2">
+            <div class="row">
+              <div class="col-md-8">
               <label>Company Email:</label>
-              <input type="text" id="company_email" value="" class="form-control">
+              <input type="text" id="company_email" value="" class="form-control form-control-sm">
+              </div>
+              <div class="col-md-4">
+              <label>Company Number:</label>
+              <input type="text" id="company_number" value="" class="form-control form-control-sm">
+              </div>
             </div>
-            
-          <button onclick="updateEmploymentDetails()" class="btn btn-primary form-control mt-2">Update Employment Info</button>  
-        </div>
-        </div>
+            <div class="row">
+              <div class="col-md-12">
+              <label>Company Address: </label>
+              <input type="text" id="company_address" value="" class="form-control form-control-sm">
+              </div>
+            </div>
+            <div class="text-end">
+            <button onclick="updateEmploymentDetails()" class="btn btn-info text-white mt-2">Update Employment Info</button>
+            </div>
+            </div>
+          </div>
         </div>
     </div>
     <form method="post">
@@ -280,7 +292,7 @@ crossorigin="anonymous" referrerpolicy="no-referrer" />
               </div>
           </div>
       </div>
-  </div>
+    </div>
 </div>
 
 </body>
