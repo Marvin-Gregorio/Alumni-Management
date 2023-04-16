6d4db70d3489106d689d3efd13fb115d2a4fc845
Leofer Dela Cruz <Leoferma099@gmail.com>
2023-04-16 16:15:26 +0800

    sub header ui change

--- pages/alumni/header.php
+++ pages/alumni/header.php
@@ -20,7 +20,7 @@
 </nav>
 
 
-<Header class="navbar navbar-expand-sm sticky-top" id="header">
+<Header class="navbar navbar-expand-sm sub-header" id="header">
 <div class="container-fluid">
 <ul class="navbar-nav mx-3">
 <li class="nav-item"><a id="nav-menu" href="job.php" class="nav-link mx-3">Jobs</a></li>
